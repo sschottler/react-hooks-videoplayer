888925918d5d18f8aba0f1239d02b083f5f7ba4e
Scott Schottler <scottschottler@gmail.com>
2019-01-04 22:17:38 -0500

    Finished

--- src/components/styles/StyledPlaylistitems.js
+++ src/components/styles/StyledPlaylistitems.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const StyledPlaylistitems = styled.div`
-  padding: 0 20px 0 20px;
+  padding: 0 20px;
   overflow-y: auto;
   height: 28vw;
   max-height: 500px;
